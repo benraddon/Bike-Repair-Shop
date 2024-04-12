07a3655b58a732b410b2fea5ad6366b2883accdb
Benjamin Raddon <154698765+benraddon@users.noreply.github.com>
2024-04-12 15:12:17 +0100

    Flashy surprise board
    An idea I came up with, just to experiment a bit. This might be a bit big for some screens, but with just a bit of simple JavaScript, I was able to make a board where you have to see what surprise you get. Simple but effective. My first original project.

---
+++ flashy board javascript/maine.js
@@ -0,0 +1,175 @@
+const griditem1 = document.getElementById('griditem1');
+
+griditem1.addEventListener('click', (event) => {
+event.target.style.backgroundColor = 'blue';
+event.target.style.color = 'white';
+event.target.textContent = `It looks like You've got Cake.🍰`;
+});
+
+griditem1.addEventListener('mouseout', (event) => {
+event.target.style.backgroundColor = 'lightblue';
+event.target.style.color = 'white';
+event.target.textContent = `Close your eyes `;
+});
+
+// FOR GRIDITEM2
+const griditem2 = document.getElementById('griditem2');
+
+griditem2.addEventListener('click', (event) => {
+event.target.style.backgroundColor = 'black';
+event.target.style.color = 'blue';
+event.target.textContent = `It looks like You've got a wave 🌊`;
+});
+
+griditem2.addEventListener('mouseout', (event) => {
+event.target.style.backgroundColor = 'lightblue';
+event.target.style.color = 'white';
+event.target.textContent = `Count to ten `;
+});
+
+// FOR GRIDITEM3
+const griditem3 = document.getElementById('griditem3');
+
+griditem3.addEventListener('click', (event) => {
+event.target.style.backgroundColor = 'red';
+event.target.style.color = 'black';
+event.target.style.textshadow = 'white';
+event.target.textContent = `It looks like You've got a car 🚗`;
+});
+
+griditem3.addEventListener('mouseout', (event) => {
+event.target.style.backgroundColor = 'lightblue';
+event.target.style.color = 'white';
+event.target.textContent = `and you'll be `;
+});
+
+const griditem51 = document.getElementById('griditem51');
+
+griditem51.addEventListener('click', (event) => {
+event.target.style.backgroundColor = 'brown';
+event.target.style.color = 'black';
+event.target.style.textshadow = 'white';
+event.target.textContent = `Have some fruit 🍏`;
+});
+
+griditem51.addEventListener('mouseout', (event) => {
+event.target.style.backgroundColor = 'lightblue';
+event.target.style.color = 'white';
+event.target.textContent = `in a world of `;
+});
+const griditem5 = document.getElementById('griditem5');
+
+griditem5.addEventListener('click', (event) => {
+event.target.style.backgroundColor = 'yellow';
+event.target.style.color = 'black';
+event.target.style.textshadow = 'white';
+event.target.textContent = `Have some sun 🌞`;
+});
+
+griditem5.addEventListener('mouseout', (event) => {
+event.target.style.backgroundColor = 'lightblue';
+event.target.style.color = 'white';
+event.target.textContent = `Pure Imagination `;
+});
+
+const griditem6 = document.getElementById('griditem6');
+
+griditem6.addEventListener('click', (event) => {
+event.target.style.backgroundColor = 'white';
+event.target.style.color = 'black';
+event.target.style.textshadow = 'white';
+event.target.textContent = `Have some snow ❄`;
+});
+
+griditem6.addEventListener('mouseout', (event) => {
+event.target.style.backgroundColor = 'lightblue';
+event.target.style.color = 'white';
+event.target.textContent = `May the Force`;
+});
+const griditem7 = document.getElementById('griditem7');
+
+griditem7.addEventListener('click', (event) => {
+event.target.style.backgroundColor = 'white';
+event.target.style.color = 'black';
+event.target.style.textshadow = 'white';
+event.target.textContent = `Have the world🌎`;
+});
+
+griditem7.addEventListener('mouseout', (event) => {
+event.target.style.backgroundColor = 'lightblue';
+event.target.style.color = 'white';
+event.target.textContent = ` Be With You  `;
+});
+
+const griditem8 = document.getElementById('griditem8');
+
+griditem8.addEventListener('click', (event) => {
+event.target.style.backgroundColor = 'white';
+event.target.style.color = 'black';
+event.target.style.textshadow = 'white';
+event.target.textContent = `Yum PIZZA🍕`;
+});
+
+griditem8.addEventListener('mouseout', (event) => {
+event.target.style.backgroundColor = 'lightblue';
+event.target.style.color = 'white';
+event.target.textContent = `I love it when...........  `;
+});
+
+const griditem10 = document.getElementById('griditem10');
+
+griditem10.addEventListener('click', (event) => {
+event.target.style.backgroundColor = 'white';
+event.target.style.color = 'black';
+event.target.style.textshadow = 'white';
+event.target.textContent = `Have some wine, a good year🍷`;
+});
+
+griditem10.addEventListener('mouseout', (event) => {
+event.target.style.backgroundColor = '#1abc9c';
+event.target.style.color = 'white';
+event.target.textContent = `A plan comes together`;
+});
+const griditem11 = document.getElementById('griditem11');
+
+griditem11.addEventListener('click', (event) => {
+event.target.style.backgroundColor = 'white';
+event.target.style.color = 'black';
+event.target.style.textshadow = 'white';
+event.target.textContent = `Let's watch a movie 🎞`;
+});
+
+griditem11.addEventListener('mouseout', (event) => {
+event.target.style.backgroundColor = 'black';
+event.target.style.color = 'white';
+event.target.textContent = `I am Vengeance .....`;
+});
+const griditem12 = document.getElementById('griditem12');
+
+griditem12.addEventListener('click', (event) => {
+event.target.style.backgroundColor = 'white';
+event.target.style.color = 'black';
+event.target.style.textshadow = 'white';
+event.target.textContent = `Let's walk on the beach 🎞`;
+});
+
+griditem12.addEventListener('mouseout', (event) => {
+event.target.style.backgroundColor = 'black';
+event.target.style.color = 'white';
+event.target.textContent = `I am the Knight...... `;
+});
+
+const griditem13 = document.getElementById('griditem13');
+
+griditem13.addEventListener('click', (event) => {
+event.target.style.backgroundColor = 'white';
+event.target.style.color = 'black';
+event.target.style.textshadow = 'white';
+event.target.textContent = `You have a new Xbox 🎞`;
+});
+
+griditem13.addEventListener('mouseout', (event) => {
+event.target.style.backgroundColor = 'black';
+event.target.style.color = 'white';
+event.target.textContent = `I am Batman `;
+});
